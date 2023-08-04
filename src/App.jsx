3395e070b4e505ec2fb5148b32686d3c295a1486
Tony Lim <tonylim9438@gmail.com>
2023-08-03 17:31:02 -0700

    first commit, chatbox ui done

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import './App.css'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator} from "@chatscope/chat-ui-kit-react";
+
+function App() {
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I am ChatGPT!",
+      sender: "ChatGPT"
+    }
+  ]) // []
+
+  const sendMessage = async (message) => {
+    const newMessage = {
+      message: message,
+      sender: "user",
+      direction: "outgoing"
+      //show on the right or sending out
+    }
+      //update our messages state (updates messagebox for user)
+
+      //process message to chatgpt and then issue a response bot
+        //updates message box again
+    const newMessages = [...messages, newMessage];
+
+    setMessages(newMessages);
+  }
+
+
+  return (
+    <div className="App">
+        <div style={{position: 'relative', height: '600px', width: '500px'}}>
+          <MainContainer> 
+            <ChatContainer> 
+              <MessageList>
+                {messages.map((message, i) => {
+                  return <Message key={i} model={message} />
+                })}
+              </MessageList>
+              <MessageInput placeholder='Type here' onSend={sendMessage}/>
+            </ChatContainer>
+          </MainContainer>
+        </div>
+    </div>
+  )
+}
+
+export default App
