9643b510b126cfd4285353d468c961bc6ed7d064
Tony Lim <tonylim9438@gmail.com>
2023-08-04 15:06:07 -0700

    third

--- src/algorithmicactivities.js
+++ src/algorithmicactivities.js
@@ -8,3 +8,21 @@ const reverseString = function(s) {
 
 let cow = ["c", "o", "w"];
 console.log(reverseString(cow));
+
+
+function lengthOfLastWord (s) {
+    let counter = 0;
+    s = s.trim();
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (s[i] !== ' ') {
+            counter++;
+        }
+        else {
+            break;
+        }
+    }
+    return counter;
+};
+
+let sentence = "hello world ";
+console.log(lengthOfLastWord(sentence)); //5
