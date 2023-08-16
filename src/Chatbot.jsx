3dc994158fbef0a45f74f5bdde9e4bee6ade3c0a
Tony Lim <tonylim9438@gmail.com>
2023-08-15 22:10:20 -0700

    moved files over, getting ready for reactrouter setup

--- src/Chatbot.jsx
+++ src/Chatbot.jsx
@@ -3,7 +3,7 @@ import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator} from "@chatscope/chat-ui-kit-react";
 
 export function Chatbot () {
-    // const API_KEY = '';;23
+    // const API_KEY = '';;234
 
   const [typing, setTyping] = useState(false);
   const [messages, setMessages] = useState([
