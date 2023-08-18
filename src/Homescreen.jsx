3d6dcd0828232095f04faf5b362b2b2da1e9a4fb
Tony Lim <tonylim9438@gmail.com>
2023-08-17 17:41:25 -0700

    added homescreen button

--- src/Homescreen.jsx
+++ src/Homescreen.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Homescreen () {
+  const navigate = useNavigate();
+  function handleClick () {
+    navigate('./Chatbot')
+  }
     return (
       <div>
-        Boom
+        <Button onClick={handleClick}> Click here to see your BOT! </Button>>
       </div>
     )
   }
