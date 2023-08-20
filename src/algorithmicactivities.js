ebfc6175bc44b57fa3ae623561b65a1eb1b57b37
Tony Lim <tonylim9438@gmail.com>
2023-08-19 18:15:55 -0700

    removed

--- src/algorithmicactivities.js
+++ src/algorithmicactivities.js
@@ -1,14 +1,3 @@
-const reverseString = function(s) {
-    let output = [];
-    for (let i = s.length-1; i >= 0; i--) {
-        output.push(s[i]);
-    }
-    return output;
-};
-
-let cow = ["c", "o", "w"];
-console.log(reverseString(cow));
-
 
 function lengthOfLastWord (s) {
     let counter = 0;
